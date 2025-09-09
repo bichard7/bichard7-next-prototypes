35615ac2e9d2f3b172085457a8c8acc11288c501
padlichan <lili.berenyi@gmail.com>
2025-09-09 13:46:16 +0100

    Add welcome page

--- login/app/routes.js
+++ login/app/routes.js
@@ -3,29 +3,32 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
 
-router.post('/sign-in', function (request, response) {
-    const {email, password} = request.body;
-    const emailValid = email.indexOf("@") > -1;
-    const correctEmail = "test@mail.com"
-    const correctPassword = "password"
-    const correctDetails = email === correctEmail && password == correctPassword
+router.post("/sign-in", function (request, response) {
+  const { email, password } = request.body;
+  const emailValid = email.indexOf("@") > -1;
+  const correctEmail = "test@mail.com";
+  const correctPassword = "password";
+  const correctDetails = email === correctEmail && password == correctPassword;
 
-    if (correctDetails) {
-        return response.redirect("/security-code")
-    }
-    return response.render("/sign-in", {emailInvalid: !emailValid, incorrectLoginDetails: true});
-})
+  if (correctDetails) {
+    return response.redirect("/security-code");
+  }
+  return response.render("/sign-in", {
+    emailInvalid: !emailValid,
+    incorrectLoginDetails: true,
+  });
+});
 
-router.post('/security-code', function (request, response) {
-    const correctSecurityCode = "1234"
-    const {securityCode} = request.body;
-    if (securityCode === correctSecurityCode) {
-        return response.redirect("/home")
-    }
-    return response.render("/security-code", {incorrectSecurityCode: true});
-})
+router.post("/security-code", function (request, response) {
+  const correctSecurityCode = "1234";
+  const { securityCode } = request.body;
+  if (securityCode === correctSecurityCode) {
+    return response.redirect("/welcome");
+  }
+  return response.render("/security-code", { incorrectSecurityCode: true });
+});
